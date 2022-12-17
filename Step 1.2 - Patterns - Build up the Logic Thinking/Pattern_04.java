670106a3bfab6b4d8671b4c9bbbdac0d38455691
Rushi <rushikeshgandhmal@gmail.com>
2022-12-17 22:44:33 +0530

    Pattern 04

--- Step 1.2 - Patterns - Build up the Logic Thinking/Pattern_04.java
+++ Step 1.2 - Patterns - Build up the Logic Thinking/Pattern_04.java
@@ -1,15 +1,15 @@
-/*
-  
-  Input: 5
-  
-  Output:
-  1
-  1 2
-  1 2 3
-  1 2 3 4
-  1 2 3 4 5
-  
- */
+/* 
+
+Input: 5
+
+Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+
+*/
 
 class Solution {
 
@@ -17,7 +17,7 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++)
-                System.out.print(j + " ");
+                System.out.print(i + " ");
             System.out.println();
         }
     }
