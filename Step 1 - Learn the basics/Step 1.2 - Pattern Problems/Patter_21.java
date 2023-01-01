b92d06fdc2fd15accf3edb7143fc35f2c6b512ba
Rushi <rushikeshgandhmal@gmail.com>
2023-01-01 23:00:11 +0530

    Pattern 21

---
+++ Step 1 - Learn the basics/Step 1.2 - Pattern Problems/Patter_21.java
@@ -0,0 +1,29 @@
+/* 
+
+Input: 4
+
+Output:
+****
+*  *
+*  *
+**** 
+
+*/
+
+class Solution {
+
+    void printSquare(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
