cd5482be34f8e9982e3d225ef997aea20a59017d
Rushi <rushikeshgandhmal@gmail.com>
2023-01-03 08:57:32 +0530

    Palindrome Number

---
+++ Step 1 - Learn the basics/Step 1.4 - Basic Maths/03_Palindrome_Number.java
@@ -0,0 +1,22 @@
+/* 
+
+Input: n = 55555
+Output: Yes
+
+*/
+
+class Solution {
+    public String is_palindrome(int n) {
+        int a = n, rev = 0;
+
+        while (a > 0) {
+
+            rev = rev * 10 + a % 10;
+
+            a /= 10;
+        }
+
+        return rev == n ? "Yes" : "No";
+    }
+
+}
