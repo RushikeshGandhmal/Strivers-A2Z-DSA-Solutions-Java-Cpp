1ad3a492300c1d6a10c96b22e59b6723f3885719
Rushi <rushikeshgandhmal@gmail.com>
2023-01-03 08:38:10 +0530

    Reverse Integer

---
+++ Step 1 - Learn the basics/Step 1.4 - Basic Maths/02_Reverse_Integer.java
@@ -0,0 +1,23 @@
+/* 
+
+Input: x = 123
+Output: 321
+
+*/
+
+class Solution {
+    public int reverse(int x) {
+
+        int rev = 0;
+
+        while (x != 0) {
+
+            if (rev > 214748364 || rev < -214748364)
+                return 0;
+
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+}
