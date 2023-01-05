c783a0176d0a99e5dc029a15d0c6b1899f598401
Rushi <rushikeshgandhmal@gmail.com>
2023-01-05 14:45:56 +0530

    Prime or Not

---
+++ Step 1 - Learn the basics/Step 1.4 - Basic Maths/07_Check_for_prime.java
@@ -0,0 +1,22 @@
+
+/* 
+
+Example 1:
+Input: N = 3
+Output: Prime
+Explanation: 3 is a prime number 
+
+*/
+
+class Solution {
+
+  public static boolean isPrime(int N) {
+
+    for (int i = 2; i < Math.sqrt(N); i++) {
+      if (N % i == 0) {
+        return false;
+      }
+    }
+    return true;
+
+  }
