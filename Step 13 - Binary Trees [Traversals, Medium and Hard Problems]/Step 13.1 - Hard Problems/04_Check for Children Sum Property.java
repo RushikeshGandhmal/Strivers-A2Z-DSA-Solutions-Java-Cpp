c124c284f11183adf9784a172222048b207cb53b
Rushi <rushikeshgandhmal@gmail.com>
2023-04-14 14:01:58 +0530

    )4_Check for Children Sum Property

--- Step 13 - Binary Trees [Traversals, Medium and Hard Problems]/Step 13.1 - Hard Problems/04_Check for Children Sum Property.java
+++ Step 13 - Binary Trees [Traversals, Medium and Hard Problems]/Step 13.1 - Hard Problems/04_Check for Children Sum Property.java
@@ -1,3 +1,17 @@
+/*
+
+ Write a program that converts any binary tree to one that follows the children sum property.
+
+The children sum property is defined as, For every node of the tree, the value of a node is equal to the sum of values of its children(left child and right child).
+
+Note: 
+
+The node values can be increased by 1 any number of times but decrement of any node value is not allowed.
+A value for a NULL node can be assumed as 0.
+You are not allowed to change the structure of the given binary tree.
+
+ */
+
 class Node {
     int data;
     Node left, right;
