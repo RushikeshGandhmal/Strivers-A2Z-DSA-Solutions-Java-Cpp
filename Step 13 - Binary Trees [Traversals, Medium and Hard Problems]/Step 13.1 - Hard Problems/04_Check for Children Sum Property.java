3a6a4ee2b20a6407c53b5ced0a76ee1a171d79dc
Rushi <rushikeshgandhmal@gmail.com>
2023-04-14 14:00:57 +0530

    )4_Check for Children Sum Property

---
+++ Step 13 - Binary Trees [Traversals, Medium and Hard Problems]/Step 13.1 - Hard Problems/04_Check for Children Sum Property.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+
+class Tree {
+    public static void changeTree(Node root) {
+        if (root == null)
+            return;
+
+        int child = 0;
+        if (root.left != null)
+            child += root.left.data;
+        if (root.right != null)
+            child += root.right.data;
+
+        if (child > root.data)
+            root.data = child;
+        else {
+            if (root.left != null)
+                root.left.data = root.data;
+            if (root.right != null)
+                root.right.data = root.data;
+        }
+
+        changeTree(root.left);
+        changeTree(root.right);
+
+        int tot = 0;
+        if (root.left != null)
+            tot += root.left.data;
+        if (root.right != null)
+            tot += root.right.data;
+        if (root.left != null || root.right != null)
+            root.data = tot;
+    }
+}
